e4de7a003862a7d2c428583ed7156d2b9b0c79d8
simplysamiul <simplysamiul@gmail.com>
2021-08-29 09:16:28 +0600

    added searching and random food area

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// ------------------ Random Food Area ------------
+const randomFoodApi = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then(res => res.json())
+    .then(data => randomFood(data.categories))
+};
+randomFoodApi();
+const randomFood = (randomFoods) => {
+    const randomFoodContainer = document.getElementById("init-card");
+    randomFoods.forEach(randomFood => {
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4");
+        div.innerHTML = `
+        <div class="card">
+          <img src="${randomFood.strCategoryThumb}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${randomFood.strCategory}</h5>
+            <p class="card-text text-muted">Fresh food is food which has not been preserved and has not spoiled yet. For vegetables and fruits, this means that they have been recently harvested and treated properly postharvest; for meat, it has recently been slaughtered and butchered.</p>
+          </div>
+        </div>
+        `
+        randomFoodContainer.appendChild(div);
+    });
+};
+// ------------------ Food Search Area ------------ 
+const getFoodInput = () =>{
+    const foodInput = document.getElementById(`food-input-main`);
+    const inputValue = foodInput.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getSearchFood(data.meals))
+    // ======= value Clear ======
+    foodInput.value = "";
+};
+const getSearchFood = foods =>{
+    const foodCard = document.getElementById("card-container");
+    document.getElementById("section-title").style.display = "block";
+    foodCard.textContent = '';
+    foods.forEach(food =>{
+        console.log(food);
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4");
+        div.innerHTML = `
+        <div class="card">
+          <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${food.strMeal.slice(1,25)}</h5>
+            <p class="card-text text-muted ">${food.strInstructions.slice(5,180)}</p>
+          </div>
+        </div>
+        `
+        foodCard.appendChild(div);
+        
+    })
+    // foodCard.appendChild(div);
+};
+document.getElementById("search-button-main").addEventListener("click", ()=>{
+    getFoodInput();
+});
+// document.getElementById("search-button-nav").addEventListener("click", ()=>{
+//     getFoodInput("nav");
+// });
